4faee40b1ad1607a082859260ea3de54a0b7e6b0
ovictorvasconcelos <victordevelopervasconcelos2006@gmail.com>
2024-06-28 13:12:15 +0100

    :sparkles: feat: set permissions for the bot (message content and members)

--- bot/bot_config.py
+++ bot/bot_config.py
@@ -3,4 +3,8 @@ from discord.ext import commands
 
 discordIntents = discord.Intents.default()
 
+#PERMISSIONS
+discordIntents.members = True
+discordIntents.message_content = True
+
 dndBot = commands.Bot(command_prefix="!", intents = discordIntents)
