c668c13d466f1ddaf7dffd42300f05e73365e2d7
ovictorvasconcelos <victordevelopervasconcelos2006@gmail.com>
2024-06-29 11:15:19 +0100

    :sparkles: feat: create the function to get the data about a certain class

--- bot/commands/dnd_commands.py
+++ bot/commands/dnd_commands.py
@@ -1,6 +1,12 @@
+import os
+import json
 import random
+import requests
+from dotenv import load_dotenv
 from discord.ext import commands
 from bot.bot_config import dndBot
+
+load_dotenv()
 
 character_sheets = {}
 
@@ -9,6 +15,22 @@ def roll_4d6():
     rolls.remove(min(rolls))
 
     return sum(rolls)
+
+#BOT COMMANDS
+@dndBot.command(name='class', aliases=['classe'], help='Show information about a class')
+async def dnd_class(context: commands.Context, *, class_name):
+    classUrl = f"{os.getenv('API_URL')}/classes/{class_name.lower()}"
+    response = requests.get(classUrl)
+
+    if response.status_code == 200:
+        classData = json.loads(response.content)
+        classDescription = classData['proficiency_choices'][0]['desc']
+
+        await context.reply(f'Class - **{classData['name']}**\n\n{classDescription}')
+    else:
+        await context.send(f'Could not find information about the class "{class_name}".')
+    
+
 
 @dndBot.command(name="create_character", help="Creates a sheet for a character")
 async def create_character(context: commands.Context, name: str, dnd_class: str, race: str):
@@ -31,8 +53,3 @@ async def create_character(context: commands.Context, name: str, dnd_class: str,
     }
 
     await context.reply(f"Character {name} the {race} {dnd_class} created successfully with attributes: {userAttributes}")
-
-@dndBot.command(name='class', aliases=['classe'], help='Show information about a class')
-async def dnd_class(context: commands.Context):
-    await context.reply('Oi')
-    
